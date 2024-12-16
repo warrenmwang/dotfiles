814fd6da35c0282690fe366980d148d50a4b25c5
Warren Wang <warren.wang2020@gmail.com>
2024-12-15 18:34:17 -0800

    nvim windows: make default term windows powershell

--- nvim/windows/init.lua
+++ nvim/windows/init.lua
@@ -11,6 +11,15 @@
 --  To update plugins you can run
 --    :Lazy update
 
+if vim.fn.has('win32') == 1 then
+  vim.opt.shell = 'powershell.exe'
+  vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+  vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+  vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+  vim.opt.shellquote = ''
+  vim.opt.shellxquote = ''
+end
+
 -- space as leader
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
