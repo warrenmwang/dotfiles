95adf48dae441783ad44dda6c9cb8bd200aca6c5
Warren Wang <warren.wang2020@gmail.com>
2024-12-22 01:45:02 -0800

    update vscode config

---
+++ vscode/export.bat
@@ -0,0 +1,3 @@
+@echo off
+@powershell cp keybindings.json $env:APPDATA\Cursor\User\keybindings.json
+@powershell cp settings.json $env:APPDATA\Cursor\User\settings.json
---
+++ vscode/import.bat
@@ -0,0 +1,3 @@
+@echo off
+@powershell cp $env:APPDATA\Cursor\User\keybindings.json keybindings.json
+@powershell cp $env:APPDATA\Cursor\User\settings.json settings.json
