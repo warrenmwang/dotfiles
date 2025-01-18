e15ca0e7b3d174749d4df8ddb8d590a0a56c8103
Warren Wang <warren.wang2020@gmail.com>
2025-01-17 22:45:54 -0800

    update vscode config

---
+++ vscode/export_settings_and_keybindings_vscode.ps1
@@ -0,0 +1,2 @@
+Copy-Item -Path "keybindings.json" -Destination "$($env:APPDATA)\Code\User\keybindings.json"
+Copy-Item -Path "settings.json" -Destination "$($env:APPDATA)\Code\User\settings.json"
---
+++ vscode/import_settings_and_keybindings_vscode.ps1
@@ -0,0 +1,2 @@
+Copy-Item -Path "$($env:APPDATA)\Code\User\keybindings.json" -Destination "keybindings.json"
+Copy-Item -Path "$($env:APPDATA)\Code\User\settings.json" -Destination "settings.json"
