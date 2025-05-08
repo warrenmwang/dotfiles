86d21872ae890a40b305c915ab00d9baa33881bb
Warren Wang <warren.wang2020@gmail.com>
2025-05-08 09:57:31 -0700

    nvim: barbar keybinds update

--- nvim/lua/custom/plugins/barbar.lua
+++ nvim/lua/custom/plugins/barbar.lua
@@ -15,11 +15,11 @@ return {
     local opts = { noremap = true, silent = true }
 
     -- Move to previous/next
-    map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-    map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+    map('n', '<A-p>', '<Cmd>BufferPrevious<CR>', opts)
+    map('n', '<A-n>', '<Cmd>BufferNext<CR>', opts)
     -- Re-order to previous/next
-    map('n', '<A-S-,>', '<Cmd>BufferMovePrevious<CR>', opts)
-    map('n', '<A-S-.>', '<Cmd>BufferMoveNext<CR>', opts)
+    map('n', '<A-S-p>', '<Cmd>BufferMovePrevious<CR>', opts)
+    map('n', '<A-S-n>', '<Cmd>BufferMoveNext<CR>', opts)
     -- Goto buffer in position...
     map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
     map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
@@ -31,8 +31,9 @@ return {
     map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
     map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
     map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+
     -- Pin/unpin buffer
-    map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+    -- map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
     -- Goto pinned/unpinned buffer
     --                 :BufferGotoPinned
     --                 :BufferGotoUnpinned
@@ -48,14 +49,14 @@ return {
     --                 :BufferCloseBuffersLeft
     --                 :BufferCloseBuffersRight
     -- Magic buffer-picking mode
-    map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+    -- map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
     -- Sort automatically by...
-    map('n', '<leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
-    map('n', '<leader>bn', '<Cmd>BufferOrderByName<CR>', opts)
-    map('n', '<leader>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-    map('n', '<leader>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-    map('n', '<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
-    map('n', '<leader>bc', '<Cmd>BufferCloseAllButCurrent<Cr>', opts)
+    -- map('n', '<leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+    -- map('n', '<leader>bn', '<Cmd>BufferOrderByName<CR>', opts)
+    -- map('n', '<leader>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+    -- map('n', '<leader>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+    -- map('n', '<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+    -- map('n', '<leader>bc', '<Cmd>BufferCloseAllButCurrent<Cr>', opts)
   end,
   version = '^1.0.0', -- optional: only update when a new 1.x version is released
 }
