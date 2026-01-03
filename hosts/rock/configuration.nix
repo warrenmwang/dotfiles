4a1283425f4cf0be13592d6c1de50eff1df8dec2
Warren Wang <warren.min.wang@gmail.com>
2026-01-02 23:33:47 -0800

    nixos: rock: enable nix-ld

--- hosts/rock/configuration.nix
+++ hosts/rock/configuration.nix
@@ -81,6 +81,8 @@
     neovim
   ];
 
+  programs.nix-ld.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
