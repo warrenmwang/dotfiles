ae6e9e6880799df4c35585a7c55571a4f4c86dd5
Warren Wang <warren.min.wang@gmail.com>
2026-02-02 00:12:07 -0800

    continue emacs configging.....

--- users/wang/config/.emacs.d/init.el
+++ users/wang/config/.emacs.d/init.el
@@ -5,6 +5,16 @@
 (ido-mode 1)
 (set-scroll-bar-mode nil)
 
+;; Adjust text to scroll in more natural feeling to me instead of the Emacs page jumps style.
+(pixel-scroll-precision-mode 1)
+(setq scroll-conservatively 101)  ; Never recenter point when scrolling
+(setq scroll-margin 0)
+(setq hscroll-step 1)
+(setq hscroll-margin 0)
+;; Horizontal scrolling with mouse.
+(setq mouse-wheel-tilt-scroll t)
+(setq mouse-wheel-scroll-amount-horizontal 5)
+
 (setq-default tab-width 4)           ; Display tabs as 4 spaces wide
 (setq-default indent-tabs-mode t)    ; Use actual tab characters
 
@@ -12,25 +22,25 @@
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "C-/") 'comment-or-uncomment-region)
 
-(setq backup-directory-alist '(("." . "~/.emacs_saves")))
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode t)
 
 ;; Disable line wrap in normal and vertical windows
 (setq-default truncate-lines t)
 (setq truncate-partial-width-windows nil)
 
-;; NOTE: this doesn't seem to do anything lmao -- Try fixing shell colors
-(require 'ansi-color)
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-(add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
+(load-theme 'modus-vivendi t)
 
 (global-auto-revert-mode 1)
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(setq backup-directory-alist '(("." . "~/.emacs_saves")))
 
 ;; TODO: keymaps for tabs (new, close, move left/right)
 ;; TODO: enable automatic inserting closing ) ] } '' might be helpful.
 
 ;; TODO: get code folding working.
-;; TODO: something is making editing slow on large files maybe
+;; TODO: something is making editing slow for large files maybe?
 ;;    copying and pasting
 ;;    creating a new line from normal mode via 'o' key.
 
@@ -54,10 +64,6 @@
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 
-(use-package idea-darkula-theme
-	:config
-	(load-theme 'idea-darkula t))
-
 (use-package evil
   :init
   (setq evil-want-C-u-scroll t)
@@ -72,6 +78,11 @@
 	(kbd "gh") 'evil-first-non-blank
 	(kbd "gl") 'evil-end-of-line
 	(kbd "<leader>o") 'dired
+	(kbd "<leader>tw") 'my-toggle-whitespace
+
+	(kbd "<leader>sf") 'counsel-projectile-find-file
+	(kbd "<leader>pp") 'counsel-projectile-switch-project
+	(kbd "<leader>pg") 'counsel-projectile-rg
   )
 
   (evil-define-key 'visual 'global
@@ -82,7 +93,8 @@
 )
 
 (define-key evil-normal-state-map "gn"
-  (lambda () (interactive) (evil-search-word-forward)))
+  (lambda () (interactive) (evil-search-word-forward))
+)
 
 (define-key evil-visual-state-map "gn"
   (lambda
@@ -93,6 +105,17 @@
 					  evil-visual-end)))
 	  (evil-exit-visual-state)
 	  (evil-search selection t t))))
+
+(use-package evil-mc
+  :config
+  (global-evil-mc-mode 1)
+  (define-key evil-normal-state-map (kbd "C-n") 'evil-mc-make-and-goto-next-match)
+  (define-key evil-visual-state-map (kbd "C-n") 'evil-mc-make-and-goto-next-match)
+  (define-key evil-normal-state-map (kbd "C-S-n") 'evil-mc-skip-and-goto-next-match)
+  (define-key evil-visual-state-map (kbd "C-S-n") 'evil-mc-skip-and-goto-next-match)
+
+  (evil-define-key 'normal evil-mc-key-map (kbd "<escape>") 'evil-mc-undo-all-cursors)
+)
 
 ;; Evil Normal Mode -- Jump/Switch to Tabs with M-N
 ;; M-1 through M-9 to switch tabs, M-0 for last one.
@@ -116,21 +139,10 @@
   (evil-define-key 'normal 'global
     (kbd "<leader>sg") 'rg
   )
-  (evil-define-key 'normal rg-mode-map
-    (kbd "<leader>l") 'rg-toggle-literal
-    (kbd "<leader>i") 'rg-toggle-case
-    (kbd "<leader>t") 'rg-toggle-filetype
+  (evil-define-key 'normal rg-mode
+	(kbd "RET") 'compile-goto-error
   )
 )
-
-(use-package web-mode
-  :mode ("\\.html\\'")
-  :config
-  (setq web-mode-markup-indent-offset 4)
-  (setq web-mode-css-indent-offset 4)
-  (setq web-mode-code-indent-offset 4)
-  (setq web-mode-enable-auto-pairing t)
-  (setq web-mode-enable-css-colorization t))
 
 (use-package ivy
   :config
@@ -142,67 +154,104 @@
   :config
   :bind (("M-x" . counsel-M-x)
          ("C-x C-f" . counsel-find-file)
-         ("<leader>sf" . counsel-find-file)
+         ;; ("<leader>sf" . counsel-find-file)
          ("<leader>SPC" . counsel-switch-buffer)
          ("<leader>sg" . counsel-rg)
   )
-)  
+)
+
+(use-package projectile
+  :config
+  (projectile-mode +1)
+  (setq projectile-completion-system 'ivy)
+  (setq projectile-enable-caching t)
+  (setq projectile-indexing-method 'native))
+(use-package counsel-projectile
+  :after projectile
+  :config
+  (counsel-projectile-mode))
+
+;; ==================== Languages START ====================
+
+(use-package web-mode
+  :mode ("\\.html\\'")
+  :config
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-css-colorization t))
+
+(use-package rust-mode
+  :mode "\\.rs\\'")
+
+;; ==================== Languages END ====================
 
 ;; TODO: It works, one kink is that sometimes the full autocompleted word doesn't maintain all the correct capitalizations of characters.
 ;; Autocomplete
-(use-package company
-  :hook (prog-mode . company-mode)
+;; (use-package company
+;;   :hook (prog-mode . company-mode)
+;;   :config
+;;   (setq company-idle-delay 0.1)
+;;   (setq company-minimum-prefix-length 2)
+
+;;   (define-key company-active-map (kbd "C-n") 'company-select-next)
+;;   (define-key company-active-map (kbd "C-p") 'company-select-previous)
+;;   (define-key company-active-map (kbd "C-y") 'company-complete-selection)
+
+;;   ;; Disable default keymaps
+;;   (define-key company-active-map (kbd "<return>") nil)
+;;   (define-key company-active-map (kbd "RET") nil)
+;;   (define-key company-active-map (kbd "TAB") nil)
+;;   (define-key company-active-map (kbd "<tab>") nil)
+;; )
+(use-package corfu
+  :init
+  (global-corfu-mode)
   :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 2)
-
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  (define-key company-active-map (kbd "C-y") 'company-complete-selection)
-
-  ;; Disable default keymaps
-  (define-key company-active-map (kbd "<return>") nil)
-  (define-key company-active-map (kbd "RET") nil)
-  (define-key company-active-map (kbd "TAB") nil)
-  (define-key company-active-map (kbd "<tab>") nil)
+  (setq corfu-auto t)                    ; Auto-popup
+  (setq corfu-auto-delay 0.1)            ; Same delay as your company setup
+  (setq corfu-auto-prefix 2)             ; Start after 2 characters
+  (setq corfu-cycle t)                   ; Enable cycling through candidates
+  
+  ;; Same keybinds as your Company config
+  (define-key corfu-map (kbd "C-n") 'corfu-next)
+  (define-key corfu-map (kbd "C-p") 'corfu-previous)
+  (define-key corfu-map (kbd "C-y") 'corfu-insert)
+  
+  ;; Disable default keymaps (like you did with Company)
+  (define-key corfu-map (kbd "RET") nil)
+  (define-key corfu-map (kbd "<return>") nil)
+  (define-key corfu-map (kbd "TAB") nil)
+  (define-key corfu-map (kbd "<tab>") nil)
 )
 
-(use-package hl-todo
-  :hook (prog-mode . hl-todo-mode)
-  :config
-  (defface my-TODO-face
-    '((t :background "#FFFF00" :foreground "#000000"))
-    "Face for TODO keywords")
+;; (use-package hl-todo
+;;   :hook (prog-mode . hl-todo-mode)
+;;   :config
+;;   (defface my-TODO-face
+;;     '((t :background "#FFFF00" :foreground "#000000"))
+;;     "Face for TODO keywords")
   
-  (defface my-FIXME-face
-    '((t :background "#FF69B4" :foreground "#000000"))
-    "Face for FIXME keywords")
+;;   (defface my-FIXME-face
+;;     '((t :background "#FF69B4" :foreground "#000000"))
+;;     "Face for FIXME keywords")
   
-  (defface my-NOTE-face
-    '((t :background "#1E90FF" :foreground "#FFFFFF"))
-    "Face for NOTE keywords")
+;;   (defface my-NOTE-face
+;;     '((t :background "#1E90FF" :foreground "#FFFFFF"))
+;;     "Face for NOTE keywords")
   
-  ;; Remove existing keywords if they exist
-  (setq hl-todo-keyword-faces
-        (cl-remove-if (lambda (x) (member (car x) '("TODO" "FIXME" "NOTE")))
-                      hl-todo-keyword-faces))
+;;   ;; Remove existing keywords if they exist
+;;   (setq hl-todo-keyword-faces
+;;         (cl-remove-if (lambda (x) (member (car x) '("TODO" "FIXME" "NOTE")))
+;;                       hl-todo-keyword-faces))
   
-  (add-to-list 'hl-todo-keyword-faces '("TODO" . my-TODO-face))
-  (add-to-list 'hl-todo-keyword-faces '("FIXME" . my-FIXME-face))
-  (add-to-list 'hl-todo-keyword-faces '("NOTE" . my-NOTE-face))
-)
+;;   (add-to-list 'hl-todo-keyword-faces '("TODO" . my-TODO-face))
+;;   (add-to-list 'hl-todo-keyword-faces '("FIXME" . my-FIXME-face))
+;;   (add-to-list 'hl-todo-keyword-faces '("NOTE" . my-NOTE-face))
+;; )
 
-(use-package evil-mc
-  :config
-  (global-evil-mc-mode 1)
-  (define-key evil-normal-state-map (kbd "C-n") 'evil-mc-make-and-goto-next-match)
-  (define-key evil-visual-state-map (kbd "C-n") 'evil-mc-make-and-goto-next-match)
-  (define-key evil-normal-state-map (kbd "C-S-n") 'evil-mc-skip-and-goto-next-match)
-  (define-key evil-visual-state-map (kbd "C-S-n") 'evil-mc-skip-and-goto-next-match)
-
-  (evil-define-key 'normal evil-mc-key-map (kbd "<escape>") 'evil-mc-undo-all-cursors)
-)
-;; NOTE: really slow issue?
+;; NOTE: really slowne issue?
 ;; (use-package diff-hl
 ;;   :config
 ;;   (global-diff-hl-mode)
@@ -231,8 +280,6 @@
 
 ;; ================ Packages END ================
 
-;; Custom Functions (commands that can be run in M-x)
-
 (defun my-counsel-rg-selection ()
   "Search for selected text with counsel-rg."
   (interactive)
@@ -241,12 +288,24 @@
                     (region-end))))
     (counsel-rg selection)))
 
+(setq whitespace-style '(face tabs spaces trailing tab-mark space-mark))
+(setq whitespace-display-mappings
+  '((space-mark 32 [183] [46])
+    (tab-mark 9 [9655 9] [92 9])))
+(defun my-toggle-whitespace ()
+  "Toggle showing whitespace characters tabs/spaces"
+  (interactive)
+  (whitespace-mode 'toggle))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(company corfu counsel counsel-projectile evil-mc hl-todo
+			 idea-darkula-theme projectile rg rust-mode smex web-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
